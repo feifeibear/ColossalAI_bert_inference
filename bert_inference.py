add63386f351af6ffd61bc2615961f59d4a2aa3d
jiaruifang <fangjiarui123@gmail.com>
2022-05-09 16:25:33 +0800

    fix the huggingface capacity bugs

--- bert_inference.py
+++ bert_inference.py
@@ -17,6 +17,65 @@ import numpy as np
 
 from transformers import BertForSequenceClassification
 
+# Hack huggingface Bert ModelOutput
+# Make it available to our ColoTensor
+from transformers.file_utils import ModelOutput
+from dataclasses import fields
+
+
+def _post_init_colotensor(self):
+    class_fields = fields(self)
+    # Safety and consistency checks
+    if len(class_fields) == 0:
+        raise ValueError(f"{self.__class__.__name__} has no fields.")
+    if not all(field.default is None for field in class_fields[1:]):
+        raise ValueError(f"{self.__class__.__name__} should not have more than one required field.")
+
+    first_field = getattr(self, class_fields[0].name)
+    other_fields_are_none = all(getattr(self, field.name) is None for field in class_fields[1:])
+
+    def is_tensor_with_colo(x):
+        """
+        Tests if `x` is a `ColoTensor` or `torch.Tensor`.
+        """
+        if isinstance(x, torch.Tensor):
+            return True
+
+        return isinstance(x, ColoTensor)
+
+    if other_fields_are_none and not is_tensor_with_colo(first_field):
+        if isinstance(first_field, dict):
+            iterator = first_field.items()
+            first_field_iterator = True
+        else:
+            try:
+                iterator = iter(first_field)
+                first_field_iterator = True
+            except TypeError:
+                first_field_iterator = False
+
+        # if we provided an iterator as first field and the iterator is a (key, value) iterator
+        # set the associated fields
+        if first_field_iterator:
+            for element in iterator:
+                if (not isinstance(element, (list, tuple)) or not len(element) == 2 or not isinstance(element[0], str)):
+                    break
+                setattr(self, element[0], element[1])
+                if element[1] is not None:
+                    self[element[0]] = element[1]
+        elif first_field is not None:
+            self[class_fields[0].name] = first_field
+    else:
+        for field in class_fields:
+            v = getattr(self, field.name)
+            if v is not None:
+                self[field.name] = v
+
+
+ModelOutput.__post_init__ = _post_init_colotensor
+# complete the hack
+
+
 def set_seed(seed):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -41,9 +100,9 @@ def run_1d_row_tp():
 
     # define the 1D Row-wise Tensor Parallel
     parallel_action_list = [
-        ParallelAction(priority=1, compute_pattern=ComputePattern.TP1DRow, parallel_mode=ParallelMode.PARALLEL_1D)
+        ParallelAction(priority=1, compute_pattern=ComputePattern.TP1DRow_Linear, parallel_mode=ParallelMode.PARALLEL_1D)
     ]
-    spec = TensorSpec(parallel_action_list)
+    linear_spec = TensorSpec(parallel_action_list)
 
     
     # A naive way to set parallel spec for all weights of Linear Op
@@ -51,7 +110,7 @@ def run_1d_row_tp():
         if not isinstance(p, ColoTensor):
             continue
         if 'weight' in name and 'LayerNorm' not in name and 'ln' not in name and 'embed' not in name:
-            p.set_spec(spec)
+            p.set_spec(linear_spec)
 
     colo_model = colo_model.cuda()
     colo_model.eval()
